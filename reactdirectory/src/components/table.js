390d4d648ae9e41a4b4c1de83c6dbf5b807e4c17
Ralds Oril <robertjswallow@gmail.com>
2021-01-25 18:40:20 -0800

    added static table with example rows and columns

--- reactdirectory/src/components/table.js
+++ reactdirectory/src/components/table.js
@@ -6,30 +6,99 @@ function Table() {
         <table className="table">
             <thead className="thead-dark">
                 <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">First</th>
-                    <th scope="col">Last</th>
-                    <th scope="col">Handle</th>
+                    <th scope="col">Image</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">DOB</th>
                 </tr>
             </thead>
             <tbody>
                 <tr>
-                    <th scope="row">1</th>
-                    <td>Mark</td>
-                    <td>Otto</td>
-                    <td>@mdo</td>
+                    <img src="https://picsum.photos/100"/>
+                    <td>Mark Otto</td>
+                    <td>555-123-4567</td>
+                    <td>motto@gmail.com</td>
+                    <td>01-19-1988</td>
                 </tr>
                 <tr>
-                    <th scope="row">2</th>
-                    <td>Jacob</td>
-                    <td>Thornton</td>
-                    <td>@fat</td>
+                <img src="https://picsum.photos/100"/>
+                    <td>Jacob Thornton</td>
+                    <td>555-123-4561</td>
+                    <td>jthornton@gmail.com</td>
+                    <td>01-20-1988</td>
                 </tr>
                 <tr>
-                    <th scope="row">3</th>
-                    <td>Larry</td>
-                    <td>the Bird</td>
-                    <td>@twitter</td>
+                <img src="https://picsum.photos/100"/>
+                    <td>Larry Bird</td>
+                    <td>555-123-4562</td>
+                    <td>lbird@gmail.com</td>
+                    <td>01-21-1988</td>
+                </tr>
+                <tr>
+                    <img src="https://picsum.photos/100"/>
+                    <td>Mark Twain</td>
+                    <td>555-123-4563</td>
+                    <td>mtwain@gmail.com</td>
+                    <td>01-22-1988</td>
+                </tr>
+                <tr>
+                <img src="https://picsum.photos/100"/>
+                    <td>Tom Cruise</td>
+                    <td>555-123-4563</td>
+                    <td>tcruise@gmail.com</td>
+                    <td>01-23-1988</td>
+                </tr>
+                <tr>
+                <img src="https://picsum.photos/100"/>
+                    <td>Mickey Mouse</td>
+                    <td>555-123-4564</td>
+                    <td>mmouse@gmail.com</td>
+                    <td>01-24-1988</td>
+
+                </tr>
+                <tr>
+                    <img src="https://picsum.photos/100"/>
+                    <td>Donald Duck</td>
+                    <td>555-123-4565</td>
+                    <td>dduck@gmail.com</td>
+                    <td>01-25-1988</td>
+                </tr>
+                <tr>
+                <img src="https://picsum.photos/100"/>
+                    <td>Ash Ketchem</td>
+                    <td>555-123-4566</td>
+                    <td>aketchem@gmail.com</td>
+                    <td>01-26-1988</td>
+                </tr>
+                <tr>
+                <img src="https://picsum.photos/100"/>
+                    <td>Gary Oak</td>
+                    <td>555-123-4511</td>
+                    <td>goak@gmail.com</td>
+                    <td>01-27-1988</td>
+                </tr>
+                <tr>
+                    <img src="https://picsum.photos/100"/>
+                    <td>Johnny Bravo</td>
+                    <td>555-123-4512</td>
+                    <td>jbravo@gmail.com</td>
+                    <td>01-28-1988</td>
+                </tr>
+                <tr>
+                <img src="https://picsum.photos/100"/>
+                    <td>Doug Funny</td>
+                    <td>555-123-4513</td>
+                    <td>dfunny@gmail.com</td>
+                    <td>01-29-1988</td>
+                </tr>
+                <tr>
+                <img src="https://picsum.photos/100"/>
+                    <td>Rodger Klotz</td>
+                    <td>555-123-4514</td>
+                    <td>mmouse@gmail.com</td>
+                    <td>01-30-1988</td>
+
                 </tr>
             </tbody>
         </table>
